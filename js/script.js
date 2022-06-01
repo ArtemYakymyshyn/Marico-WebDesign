2ed5107046e7e33028f37733159d40dc1df598f6
ArtemYakymyshyn <artem.yakymyshyn.2016@gmail.com>
2022-06-01 14:40:17 +0300

    all the first and last lines changed + hover added to all html tagged buttons

--- js/script.js
+++ js/script.js
@@ -15,7 +15,42 @@ $fsBurgerMenuWraper.addEventListener('click', () => {
 const buttons = document.querySelectorAll('button')
 for (let i = 0; i < buttons.length; i++) {
    buttons[i].classList.add('baton-activ');
-}
+   let buttonsColors = buttons[i].style.background;
+   let buttonTextColors = buttons[i].style.color;
+   let buttonLetterSpacing = buttons[i].style.letterSpacing;
+   let buttonWidth = buttons[i].style.width;
+   let buttonTextShadown = buttons[i].style.textShadow
+   const calculateWidth = (e) => {
+
+      let buttonNewWidth = buttonWidth * '10%'
+      return buttonNewWidth;
+   };
+   buttons[i].addEventListener('mouseenter', (e) => {
+      buttons[i].style.background = 'crimson';
+      buttons[i].style.color = 'white';
+      buttons[i].style.transitionDuration = '0.5s'
+      buttons[i].style.letterSpacing = '2.8px'
+      buttons[i].style.width = calculateWidth();
+      buttons[i].style.textShadow = '#FFF 0px 0px 5px, #FFF 0px 0px 10px, #FFF 0px 0px 15px, #FF2D95 0px 0px 20px, #FF2D95 0px 0px 30px, #FF2D95 0px 0px 40px, #FF2D95 0px 0px 50px, #FF2D95 0px 0px 75px'
+
+   })
+   buttons[i].addEventListener('mouseleave', function (e) {
+      buttons[i].style.background = buttonsColors;
+      buttons[i].style.color = buttonTextColors;
+      buttons[i].style.letterSpacing = buttonLetterSpacing
+      buttons[i].style.width = buttonWidth;
+      buttons[i].style.textShadow = buttonTextShadown
+   });
+};
+
+const delegatingList = document.querySelector('.lal-element-2-list');
+delegatingList.addEventListener('mouseenter', function (e) {
+   if (e.target.hasAttribute('data-set')) {
+
+   }
+});
+
+
 
 
 
